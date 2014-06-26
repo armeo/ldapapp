1c18760f56f2a58f30b493a160e8a37152ce690f
adisorn <armchockaumnuai@gmail.com>
2014-06-26 15:55:00 +0700

    test connection and get email

--- src/main/java/com/ldap/LdapFactory.java
+++ src/main/java/com/ldap/LdapFactory.java
@@ -1,13 +1,40 @@
 package com.ldap;
 
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import java.util.Hashtable;
+
 /**
  * Created by Adisorn Chockaumnuai on 6/25/2014.
  */
 public class LdapFactory {
+
+    private String principal;
+    private String credential;
+
     public LdapFactory(String user, String password) {
+        this.principal = user;
+        this.credential = password;
     }
 
-    public Object getConnection() {
-        return null;
+    public DirContext getConnection() {
+        Hashtable<Object, Object> env = new Hashtable<Object, Object>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldap://oid.fico.com:3060");
+        env.put(Context.SECURITY_PRINCIPAL, this.principal);
+        env.put(Context.SECURITY_CREDENTIALS, this.credential);
+
+        try {
+            return new InitialDirContext(env);
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            return null;
+        }
+    }
+
+    public String getEmail(String user) {
+        return "orcl@mail.com";
     }
 }
--- src/test/java/com/ldap/LdapFactoryTest.java
+++ src/test/java/com/ldap/LdapFactoryTest.java
@@ -2,8 +2,10 @@ package com.ldap;
 
 import org.junit.Test;
 
+import javax.naming.NamingException;
+
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by Adisorn Chockaumnuai on 6/25/2014.
@@ -11,11 +13,29 @@ import static org.junit.Assert.*;
 public class LdapFactoryTest {
 
     @Test
-    public void shouldNullWithWrongUsernamePassword(){
+    public void shouldNullWithWrongUsernamePassword() throws NamingException {
         String user = "aaa";
         String password = "xxx";
         LdapFactory ldap = new LdapFactory(user, password);
 
         assertThat(ldap.getConnection(), is(nullValue()));
+    }
+
+    @Test
+    public void shouldNotNullWithRightUsernamePassword() throws NamingException {
+        String user = "cn=orcladmin";
+        String password = "OID#dm1n";
+        LdapFactory ldap = new LdapFactory(user, password);
+
+        assertThat(ldap.getConnection(), is(notNullValue()));
+    }
+
+    @Test
+    public void shouldReturnEmailWhenUserExists(){
+        String user = "cn=orcladmin";
+        String password = "OID#dm1n";
+        LdapFactory ldap = new LdapFactory(user, password);
+
+        assertThat(ldap.getEmail(user), is("orcl@mail.com"));
     }
 }
